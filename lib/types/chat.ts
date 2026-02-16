dbccdfe8dc0f5d423973febd75ce3fa136680cbf
Kristian <kristianfischer29@gmail.com>
2026-02-16 13:52:39 -0500

    fix(api): update chat type and dto schema

--- lib/types/chat.ts
+++ lib/types/chat.ts
@@ -1,16 +1,8 @@
 export interface ChatResponse {
-        response: string,
-        metadata: {
-            input_tokens: number,
-            output_tokens: number,
-            new_tokens: number,
-            generation_time: number,
-            tokens_per_second: number,
-            model: string,
-            parameters:  {
-                temperature: number,
-                max_length: number,
-                top_p: number,
-            }
-    }
+  response: string;
+  metadata: {
+    ragUsed: boolean;
+    contextLength: number;
+    promptLength: number;
+  };
 }
